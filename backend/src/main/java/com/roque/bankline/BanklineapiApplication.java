2ad7f446e3b9945d6ec5966c800877ad48a4b2d1
lucasfroque <lucasfernandolf38@gmail.com>
2022-05-11 23:28:37 -0300

    Projeto criado

---
+++ backend/src/main/java/com/roque/bankline/BanklineapiApplication.java
@@ -0,0 +1,13 @@
+package com.roque.bankline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/roque/bankline/BanklineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roque.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
